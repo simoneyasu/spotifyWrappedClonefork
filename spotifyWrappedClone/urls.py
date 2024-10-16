aff01fd6653bd8602a34e81946ad75d7f5a6163f
Jcloer624 <jcloer624@gmail.com>
2024-10-16 16:24:15 -0400

    I'm trying something

--- spotifyWrappedClone/urls.py
+++ spotifyWrappedClone/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('register/', include('register.urls')),
 ]
