023dabf5ef91dc8c4f1c0abd1c724d5cca71dc11
Derrick <derrickchen0201@gmail.com>
2024-11-14 13:50:27 -0500

    Added share to Twitter and LinkedIn to analyze_wrap.html

--- wrap/views.py
+++ wrap/views.py
@@ -1,11 +1,9 @@
-import uuid
-
 from django.shortcuts import render, redirect, get_object_or_404
-
 from functionality.views import get_User_Data
 import openai
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
+
 from register.models import SpotifyWrap
 
 '''
