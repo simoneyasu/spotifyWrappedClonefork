3e028f931db17a3e4732c5cb3bab9ee712a7600f
Derrick <derrickchen0201@gmail.com>
2024-12-01 21:30:09 -0500

    Revert "please don't break"
    This reverts commit 6a237954b7315dfbd0ebb40761786605ee912899.

--- wrap/urls.py
+++ wrap/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from register.views import check_token_scopes
 from wrap import views
 from register import views as register_views
 
@@ -9,18 +10,10 @@ urlpatterns = [
     path('wrap_detail/<uuid:wrap_id>/', views.wrap_detail, name='wrap_detail'),
     path('wrap/<uuid:wrap_id>/analyze/', views.analyze_wrap, name='analyze_wrap'),
     path('wrap/<uuid:wrap_id>/delete/', views.delete_wrap, name='delete_wrap'),
-    path('create/', views.create, name='create_wrap'),
+    path('wrap/create/', views.create, name='create_wrap'),
 
     # Spotify OAuth
     path('auth/spotify/callback/', register_views.spotify_callback, name='spotify_callback'),
-
     # LinkedIn OAuth
-    path('auth/linkedin/', views.linkedin_login, name='linkedin_login'),
-    path('auth/linkedin/callback/', views.linkedin_callback, name='linkedin_callback'),
-
-    # Twitter OAuth
-    path('auth/twitter/login/', views.twitter_login, name='twitter_login'),
-    path('auth/twitter/callback/', views.twitter_callback, name='twitter_callback'),
-    path('upload-to-linkedin/', views.upload_to_linkedin, name='upload_to_linkedin'),
-    path('upload-to-twitter/', views.upload_to_twitter, name='upload_to_twitter'),
+    path('callback/', register_views.spotify_callback, name='spotify_callback'),
 ]
